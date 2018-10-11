3ec75b3bd88d178ebbab55606509507acfba2af1
vdidk87 <verenok@gmail.com>
2018-10-11 16:38:53 +0300

    minor changes

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,15 @@
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class Account {
     private int balance;
     private Lock lock;
+    private AtomicInteger failCounter = new AtomicInteger(0);
+
+    void incFailedTransferCount() {
+        failCounter.incrementAndGet();
+    }
 
     Account(int balance) {
         this.balance = balance;
@@ -24,5 +30,9 @@ class Account {
 
     Lock getLock() {
         return lock;
+    }
+
+    AtomicInteger getFailCounter() {
+        return failCounter;
     }
 }
